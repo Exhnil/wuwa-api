e2fc586fa1afd3fdc9098b8e3742533807f288eb
Exhnil <g.confiac@live.fr>
2025-09-25 02:40:57 +0900

    made data endpoint type/id of object, added listing objects of a type and returning json file

--- src/module/filesystem.js
+++ src/module/filesystem.js
@@ -1,14 +1,40 @@
-import fs from "fs";
+import { promises as fs, existsSync } from "fs";
 import path from "path";
-import { fileURLToPath } from "url";
 
 const dataDir = path.join(process.cwd(), "/assets/data");
 
-function exist(p) {
-  return fs.existsSync(p);
+export async function containsFolders(path) {
+  const folder = await fs.readdir("assets/data/" + path, {
+    withFileTypes: true,
+  });
+  console.log("assets/data/" + path);
+  console.log(folder[0].isDirectory());
+  return folder[0].isDirectory();
 }
 
 export async function getTypes() {
   const types = await fs.readdirSync(dataDir);
   return types;
 }
+
+export async function getAvailableEntities(type) {
+  const entities = await fs.readdir(path.join(dataDir, type));
+  console.log(entities);
+  return entities;
+}
+
+export async function getEntity(type, id) {
+  const filePath = path.join(dataDir, type, id, id + ".json").normalize();
+
+  const exists = existsSync(filePath);
+
+  if (!exists) {
+    return "404 not found";
+  }
+
+  const file = await fs.readFile(filePath);
+  const entity = JSON.parse(file.toString('utf-8'))
+  return entity
+}
+
+export async function getImage() {}
--- src/routes/routes.js
+++ src/routes/routes.js
@@ -1,10 +1,31 @@
 import { Router } from "express";
-import { getTypes } from "../module/filesystem.js";
+import {
+  getTypes,
+  containsFolders,
+  getAvailableEntities,
+  getEntity,
+} from "../module/filesystem.js";
 
 const router = Router();
 
 router.get("/", async (req, res) => {
   res.json({ types: await getTypes() });
+});
+
+router.get("/:type/{:id}", async (req, res) => {
+  const { type, id } = req.params;
+
+  const fullPath = id ? type + "/" + id : type;
+
+  console.log(fullPath);
+
+  if (await containsFolders(fullPath)) {
+    const entities = await getAvailableEntities(fullPath);
+    res.json(entities);
+  } else {
+    const entity = await getEntity(type, id);
+    res.json(entity);
+  }
 });
 
 export default router;
