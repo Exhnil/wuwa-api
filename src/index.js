93540bf07ed2df8d8f87d42db8588ab410857990
Exhnil <g.confiac@live.fr>
2025-10-06 03:50:24 +0900

    removed limiter. renamed bad files

--- src/index.js
+++ src/index.js
@@ -4,7 +4,6 @@ import routes from "./routes/routes.js";
 import cors from "cors";
 import morgan from "morgan";
 import helmet from "helmet";
-import rateLimit from "express-rate-limit";
 import fs from "fs";
 import path from "path";
 
@@ -22,18 +21,15 @@ if (process.env.NODE_ENV !== "development") {
 
 app.use(helmet());
 
-const limiter = rateLimit({
-  windowMs: 15 * 60 * 1000,
-  max: 100,
-});
+app.use(cors());
 
-app.use(limiter);
+app.use("/", routes);
 
-const logFile = Path2D.join(__dirname, "errors.log");
+const logFile = path.join(process.cwd(), "errors.log");
 
 app.use((error, req, res, next) => {
-  console.error(err.stack);
-  const log = `[${new Date().toISOString()}] ${err.stack}\n`;
+  console.error(error.stack);
+  const log = `[${new Date().toISOString()}] ${error.stack}\n`;
   fs.appendFile(logFile, log, (e) => {
     if (e) console.error("Failed to write log", e);
   });
@@ -45,24 +41,16 @@ app.use((error, req, res, next) => {
   });
 });
 
-app.use(
-  cors({
-    origin: "*",
-  })
-);
-
-app.use("/", routes);
-
 app.listen(PORT, () => {
   console.log(`Listening on ${PORT}...`);
 });
 
 process.on("unhandledRejection", (reason, promise) => {
-  console.err("Unhandled Rejection:", reason);
+  console.error("Unhandled Rejection:", reason);
   process.exit(1);
 });
 
 process.on("uncaughtException", (err) => {
-  console.err("Unhandled Exception:", err);
+  console.error("Unhandled Exception:", err);
   process.exit(1);
 });
